de26130f4758a4c407ec7f9e98191e31d1d7ca4a
Ag <puchetatome.angelica@gmail.com>
2023-11-08 12:17:02 -0600

    added styles to menu sidebar of nav.

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,4 @@
 const menuHamburguer = document.querySelector('.nav__hamburguer');
-const overlaySideBarMenu = document.querySelector('.nav__overlay');
 
 menuHamburguer.addEventListener('click', function(e){
     menuHamburguer.classList.toggle('nav__hamburguer--open');
