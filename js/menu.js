e2a679e0951fccc7227f8d0da983c1afe5ac5514
Ag <puchetatome.angelica@gmail.com>
2023-11-09 21:53:35 -0600

    showing and hiding menu sidebar.

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
 const menuHamburguer = document.querySelector('.nav__hamburguer');
+const menuOverlay = document.querySelector('.nav__overlay');
+
 
 menuHamburguer.addEventListener('click', function(e){
     menuHamburguer.classList.toggle('nav__hamburguer--open');
+
+    //Show menu sidebar nav
+    menuOverlay.classList.toggle('nav__overlay--open');
 });
+
+
+
