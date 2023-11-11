4b51e4b1ca7649b356c4d0d4448eb380ecc8b655
Ag <puchetatome.angelica@gmail.com>
2023-11-10 18:00:29 -0600

    dropdown list menu sidebar with js.

--- js/menu.js
+++ js/menu.js
@@ -9,5 +9,30 @@ menuHamburguer.addEventListener('click', function(e){
     menuOverlay.classList.toggle('nav__overlay--open');
 });
 
+menuOverlay.addEventListener('click', function(e){
+    const elementTarget = e.target;
+    // console.log(e.target.classList.value)
+
+    if(isActive(elementTarget, 'nav__parent') ){
+        //parentElement: Acceder al padre(nav__item) del elemento actual(nav__parent)
+        //Children(ArrayHTMLCollection): Acceder a sus hijos(span, ul)
+        const subMenu = elementTarget.parentElement.children[1];
+        console.log(subMenu);
+
+        if(window.innerWidth < 768){
+
+            // `¿Height actual de submenu es = 0?` SI: el Height ahora es igual al ScrollHeight(alto mínimo que se le da a un elemento para que exista)   
+            let heightSubMenu = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
+
+            subMenu.style.height = `${heightSubMenu}px`
+        }else{
+       
+        }
+    }
+})
 
 
+function isActive(element, string){
+    
+    return element.classList.value.includes(string);;
+}
